80f887bb0465e2891c658f77d054d43df03cdbaa
Алисултанов Сафарали Энверович <safarali.alisultanov@norbit.ru>
2016-10-14 12:25:20 +0300

    Сделал:   - метод получения недели с Пн по Вс по заданной дате.

--- app/src/main/java/ru/norbit/pageview/MainActivity.java
+++ app/src/main/java/ru/norbit/pageview/MainActivity.java
@@ -7,11 +7,20 @@ import android.app.Fragment;
 //import android.support.v4.view.PagerAdapter;
 //import android.support.v4.view.ViewPager;
 import android.app.FragmentManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends FragmentActivity {
 
@@ -25,6 +34,19 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar c = Calendar.getInstance();
+
+        // Индекс дня недели
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        String weekday = new DateFormatSymbols().getShortWeekdays()[dayOfWeek];
+//        Log.e(LOG_TAG, "День Недели2 ->" + weekday);
+
+        // Получить неделю с Пн по Вс
+        String[] days = getWeekOfMonth(14,Calendar.OCTOBER,2016);
+        for (int i = 0; i < days.length; i++) {
+            Log.e(LOG_TAG, days[i]);
+        }
 
         pager = (ViewPager)findViewById(R.id.pager);
         pagerAdapter = new MyFragmentPagerAdapter(getFragmentManager());
@@ -46,6 +68,28 @@ public class MainActivity extends FragmentActivity {
         });
     }
 
+    public static String[] getWeekOfMonth(int day, int month, int year) {
+        Calendar now = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+        String[] days = new String[7];
+
+        now.set(year, month, day);
+        int dayOfWeek = now.get(Calendar.DAY_OF_WEEK);
+        String weekday = new DateFormatSymbols().getShortWeekdays()[dayOfWeek];
+//        Log.e(LOG_TAG, "День Недели2 ->" + weekday);
+//        Log.e(LOG_TAG, "dayOfWeek -> " + String.valueOf(dayOfWeek));
+//        Log.e(LOG_TAG, "DAY_OF_WEEK -> " + now.get(GregorianCalendar.DAY_OF_WEEK));
+        int delta = -(now.get(GregorianCalendar.DAY_OF_WEEK)-2);
+        if (delta > 0)
+            delta = -6;
+        now.add(Calendar.DAY_OF_MONTH, delta);
+        for (int i = 0; i < 7; i++) {
+            days[i] = sdf.format(now.getTime());
+            now.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        return days;
+    }
+
     private class MyFragmentPagerAdapter extends MyFragmentStatePagerAdapter {
 
         public MyFragmentPagerAdapter(FragmentManager fm) {
@@ -59,7 +103,7 @@ public class MainActivity extends FragmentActivity {
 
         @Override
         public int getCount() {
-            Log.e(LOG_TAG, "Count page_view -> " + String.valueOf(100));
+//            Log.e(LOG_TAG, "Count page_view -> " + String.valueOf(100));
             return 100;
         }
     }
